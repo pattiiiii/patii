9f4f0f3d731814a6d9f6e60629bae65e82869d1c
pattiiiii <100131924+pattiiiii@users.noreply.github.com>
2022-02-22 19:15:27 +0800

    Create patti4.cpp

---
+++ patti4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int myAge = 14;
+  cout << " I am " << 14 << " years old.";
+  cout << " my height " << 151 << " cm.";
+  cout << " my weight " << 49  << " kg.";
+  cout << " i have " << 1 << " brother.";
+  cout << " i have " << 0 << " sister.";
+  return 0;
+}
