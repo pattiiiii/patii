5157805415ba868367b16fc18ec2587658988ed5
pattiiiii <100131924+pattiiiii@users.noreply.github.com>
2022-02-22 19:57:33 +0800

    Update patti4.cpp

--- patti4.cpp
+++ patti4.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 using namespace std;
-
 int main() {
   int myAge = 14;
-  cout << " I am " << 14 << " years old.";
-  cout << " my height " << 151 << " cm.";
-  cout << " my weight " << 49  << " kg.";
-  cout << " i have " << 1 << " brother.";
-  cout << " i have " << 0 << " sister.";
+  int myheight = 151;
+  int myweight =49;
+  int mybrither = 1;
+  int mysister = 0;
+  cout << " I am " << myAge << " years old.\n";
+  cout << " my height " << myheight << " cm.\n";
+  cout << " my weight " << myweight  << " kg.\n";
+  cout << " i have " << mybrither << " brother.\n";
+  cout << " i have " << mysister << " sister.\n";
   return 0;
 }
